5b61ab83adc1b21355ee0a50f4aac52a97b4d403
Diego Alejandro Arévalo <diego.arevalo.97@hotmail.com>
2019-02-23 20:49:58 -0600

    update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,17 +3,11 @@ const { Router } = require('express');
 const router = Router();
 
 // Cargamos la rutas de los modulos
-const RutaUsuario = require('./usuario');
-const RutaUnidad = require('./unidad');
-const RutaDeportista = require('./deportista');
 
 router.get('/', (req, res) => {
   res.send('Bienvenido a Inicio!');
 });
 
 // Cargamos la rutas de los modulos
-router.use('/usuario',RutaUsuario);
-router.use('/unidad',RutaUnidad);
-router.use('/deportista',RutaDeportista);
 
 module.exports = router;
