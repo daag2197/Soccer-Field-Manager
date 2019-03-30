5c5812dca99b21f2c8a863c333aa01cfbdc8fc05
Diego Alejandro Arévalo <diego.arevalo.97@hotmail.com>
2019-03-29 20:50:33 -0600

    Seeder de Usuario
    Creacion de seeder de usuarios

---
+++ src/seeders/20190330024514-demo-user.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('user', [{
+      FirstName: 'Diego Alejandro',
+      LastName: 'Arevalo',
+      SecondLastName: 'Garcia',
+      Email: 'diego.arevalo.97@hotmail.com',
+      Password: '1234',
+      UserType: '1',
+      createdAt: '2019-03-04 04:56:55',
+      updatedAt: '2019-03-04 04:56:55'
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('user', null, {});
+  }
+};
